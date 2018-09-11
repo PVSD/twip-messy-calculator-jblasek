7e474197cf08ef8e06d754354dcce8127287d502
jb838 <jacob.blasek1220@gmail.com>
2018-09-11 11:22:47 -0400

    AHHH PANIC

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,37 @@
 package com.company;
+import java.util.Scanner;
 
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kbReadingString = new Scanner(System.in);
+        Scanner kbReadingInt = new Scanner(System.in);
+
+
+        System.out.println("Do you want to add, subtract, divide, or multiply.");
+
+       String sign = kbReadingString.next();
+        System.out.println("Type in your two numbers.");
+
+        int num1 = kbReadingInt.nextInt();
+        int num2 = kbReadingInt.nextInt();
+
+        String signWord = "";
+
+        if (sign == "add") {
+            signWord = "sum";
+        }
+        else if (sign == "subtract") {
+            signWord = "difference";
+        }
+        else if (sign == "multiply") {
+            signWord = "product";
+        }
+        else if (sign == "divide"){
+            signWord = "dividend";
+        }
+        System.out.println("The" + signWord + "of " + num1 + "and" + num2);
+
     }
 }
